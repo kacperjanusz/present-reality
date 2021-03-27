2c411dd2dcd4b1d73b9effc995070ca37c5f1509
kjanu <kjanusz@goldencomm.com>
2021-03-27 23:22:42 +0100

    cleaning xD

--- src/Pages/Login/LoginStyles.js
+++ src/Pages/Login/LoginStyles.js
@@ -10,15 +10,3 @@ export const Box = styled.div`
   background-color: #10305f;
   padding: 5;
 `;
-
-export const Card = styled.div`
-  width: 50vw;
-  height: 50vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #fbf7f7;
-  padding: 5;
-  border-radius: 5;
-`;
