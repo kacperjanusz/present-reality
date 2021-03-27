4b94a02ab7ddec28efa897fefe6ba3316a228986
KlaraGajaszek <klaragajaszek21@wp.pl>
2021-03-28 00:24:23 +0100

    add function to change page

--- src/Components/LoginCard/LoginCard.js
+++ src/Components/LoginCard/LoginCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { loginTitle, next } from '../../constants';
+import { useHistory } from 'react-router-dom';
 import { CardWrapper, Title, CustomInput, CustomButton } from './LoginCardStyles';
 
 export const LoginCard = () => {
+  const history = useHistory();
   const [value, setValue] = React.useState(localStorage.getItem('myValueInLocalStorage') || '');
 
   React.useEffect(() => {
@@ -14,8 +16,15 @@ export const LoginCard = () => {
   return (
     <CardWrapper>
       <Title>{loginTitle}</Title>
-      <CustomInput style={{width: '50%'}} value={value} type="text" onChange={onChange} placeholder="Name" />
-      <CustomButton type="primary">{next}</CustomButton>
+      <CustomInput style={{ width: '50%' }} value={value} type="text" onChange={onChange} placeholder="Name" />
+      <CustomButton
+        type="primary"
+        onClick={() => {
+          history.push('/information');
+        }}
+      >
+        {next}
+      </CustomButton>
     </CardWrapper>
   );
 };
