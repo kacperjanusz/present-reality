23f19ff0ad9012381769f26a4943169fb42deb90
kjanu <kjanusz@goldencomm.com>
2021-03-27 23:20:36 +0100

    login update

---
+++ src/Pages/Login/LoginStyles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Box = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #10305f;
+  padding: 5;
+`;
+
+export const Card = styled.div`
+  width: 50vw;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fbf7f7;
+  padding: 5;
+  border-radius: 5;
+`;
--- src/constanst.js
+++
@@ -1,6 +0,0 @@
-export const mainQuote = "Tak naprawdę nigdy nie było szybszego i potężniejszego sposobu propagowania kłamstw niż opublikowanie ich online"
-export const authorMainQuote = "~ Matthew D'Ancona"
-export const trainYourAwareness = "Trenuj swoją świadomość"
-export const ItsTimeToGetToKnowYourMins = "Pora poznać swój umysł"
-export const mainTextQuiz = "Przed tobą quiz składający się z 10 zadań."
-export const mainDescriptionQuiz = "Na każde zadanie masz 10 sekund. Po zakończeniu quizu okaże się czy jesteś wirtualnym cyborgiem czy jeszcze realnym człowiekem. Jeżeli już zostałeś królem świata wirtualnego nie martw siępomożemy Ci wrócić do realnego świata"
