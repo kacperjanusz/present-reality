300fd005dbdd82fab665f8994f092cfba2793b20
KlaraGajaszek <klaragajaszek21@wp.pl>
2021-03-27 18:31:08 +0100

    add css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { AppRoutes } from './routing/AppRoutes'
+import { AppRoutes } from './routing/AppRoutes';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <>
       <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
